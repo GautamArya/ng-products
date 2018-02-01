1f6876fbf238f4e44442621a26dfc157491eaed4
Gautam <gautam@prakashinfotech.com>
2018-02-01 16:48:01 +0530

    currency format, service injecting.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductService } from './products/product.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<app-pm-products></app-pm-products>`,
+  styleUrls: ['./app.component.css'],
+  providers: [ ProductService ]
 })
 export class AppComponent {
   pageTitle = 'Product App';
